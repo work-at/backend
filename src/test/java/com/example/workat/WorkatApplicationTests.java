ee402c95909956b7a5531f4f4271d133daca1ebf
SeungYoung Oh <seung-00@naver.com>
2022-06-07 12:21:56 +0900

    feat: 스프링부트 프로젝트 셋업

---
+++ src/test/java/com/example/workat/WorkatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
