9f1f07cc6e5e4c31fe19fe430fd7865b58002834
Soyeon Park <sypark9646@gmail.com>
2022-06-28 01:49:32 +0900

    feat: create RestTemplate connection pool

---
+++ src/main/java/com/workat/domain/config/RestTemplateConfig.java
@@ -0,0 +1,46 @@
+package com.workat.domain.config;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+	@Value("${restTemplate.factory.readTimeout}")
+	private int READ_TIMEOUT;
+
+	@Value("${restTemplate.factory.connectTimeout}")
+	private int CONNECT_TIMEOUT;
+	@Value("${restTemplate.httpClient.maxConnTotal}")
+	private int MAX_CONN_TOTAL;
+	@Value("${restTemplate.httpClient.maxConnPerRoute}")
+	private int MAX_CONN_PER_ROUTE;
+
+	@Bean
+	HttpClient httpClient() {
+		return HttpClientBuilder.create()
+			.setMaxConnTotal(MAX_CONN_TOTAL)
+			.setMaxConnPerRoute(MAX_CONN_PER_ROUTE)
+			.build();
+	}
+
+	@Bean
+	HttpComponentsClientHttpRequestFactory factory(HttpClient httpClient) {
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		factory.setReadTimeout(READ_TIMEOUT);
+		factory.setConnectTimeout(CONNECT_TIMEOUT);
+		factory.setHttpClient(httpClient);
+
+		return factory;
+	}
+
+	@Bean
+	RestTemplate restTemplate(HttpComponentsClientHttpRequestFactory factory) {
+		return new RestTemplate(factory);
+	}
+}
