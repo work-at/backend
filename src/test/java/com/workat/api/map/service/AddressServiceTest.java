4907e67389d2013d29557bd14355b3fb690dec51
Soyeon Park <sypark9646@gmail.com>
2022-06-30 21:03:12 +0900

    test: mock service test

--- src/test/java/com/workat/api/map/service/AddressServiceTest.java
+++ src/test/java/com/workat/api/map/service/AddressServiceTest.java
@@ -2,31 +2,85 @@ package com.workat.api.map.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.web.client.RestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.BDDMockito.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.workat.domain.map.entity.WorkerLocation;
+import com.workat.domain.map.http.LocationHttpReceiver;
+import com.workat.domain.map.http.dto.KakaoAddressResponse;
+import com.workat.domain.map.repository.WorkerLocationRedisRepository;
 
-@ActiveProfiles("test")
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class AddressServiceTest {
 
-	@Autowired
+	@Mock
+	private LocationHttpReceiver locationHttpReceiver;
+
+	@Mock
+	private WorkerLocationRedisRepository workerLocationRedisRepository;
+
+	@InjectMocks
 	private AddressService addressService;
 
+	private final ObjectMapper mapper = new ObjectMapper();
+
 	@Test
-	void testSaveAddress() {
+	void testSaveAddress() throws JsonProcessingException {
 		//given
+		WorkerLocation workerLocation = WorkerLocation.of("123", "127.423084873712", "37.0789561558879", "경기 안성시 죽산면 죽산리");
+		String responseString = "{\n"
+			+ "    \"meta\": {\n"
+			+ "        \"total_count\": 1\n"
+			+ "    },\n"
+			+ "    \"documents\": [\n"
+			+ "        {\n"
+			+ "            \"road_address\": {\n"
+			+ "                \"address_name\": \"경기도 안성시 죽산면 죽산초교길 69-4\",\n"
+			+ "                \"region_1depth_name\": \"경기\",\n"
+			+ "                \"region_2depth_name\": \"안성시\",\n"
+			+ "                \"region_3depth_name\": \"죽산면\",\n"
+			+ "                \"road_name\": \"죽산초교길\",\n"
+			+ "                \"underground_yn\": \"N\",\n"
+			+ "                \"main_building_no\": \"69\",\n"
+			+ "                \"sub_building_no\": \"4\",\n"
+			+ "                \"building_name\": \"무지개아파트\",\n"
+			+ "                \"zone_no\": \"17519\"\n"
+			+ "            },\n"
+			+ "            \"address\": {\n"
+			+ "                \"address_name\": \"경기 안성시 죽산면 죽산리 343-1\",\n"
+			+ "                \"region_1depth_name\": \"경기\",\n"
+			+ "                \"region_2depth_name\": \"안성시\",\n"
+			+ "                \"region_3depth_name\": \"죽산면 죽산리\",\n"
+			+ "                \"mountain_yn\": \"N\",\n"
+			+ "                \"main_address_no\": \"343\",\n"
+			+ "                \"sub_address_no\": \"1\",\n"
+			+ "                \"zip_code\": \"\"\n"
+			+ "            }\n"
+			+ "        }\n"
+			+ "    ]\n"
+			+ "}";
+		JsonNode responseJsonNode = mapper.readTree(responseString);
+		KakaoAddressResponse kakaoResponse = mapper.treeToValue(responseJsonNode, KakaoAddressResponse.class);
+		Mockito.when(locationHttpReceiver.getAddress("127.423084873712", "37.0789561558879")).thenReturn(kakaoResponse);
+		Mockito.when(workerLocationRedisRepository.save(any())).thenReturn(workerLocation);
+
+		//when
 		WorkerLocation saved = addressService.saveAddress("123", "127.423084873712", "37.0789561558879");
 
 		//then
 		Assertions.assertAll(
-			() -> Assertions.assertEquals(saved.getUserId(), "123"),
-			() -> Assertions.assertEquals(saved.getLatitude(), "37.0789561558879"),
-			() -> Assertions.assertEquals(saved.getLongitude(), "127.423084873712"),
-			() -> Assertions.assertEquals(saved.getAddress(), "경기 안성시 죽산면 죽산리")
+			() -> Assertions.assertEquals(saved.getUserId(), workerLocation.getUserId()),
+			() -> Assertions.assertEquals(saved.getLongitude(), workerLocation.getLongitude()),
+			() -> Assertions.assertEquals(saved.getLatitude(), workerLocation.getLatitude()),
+			() -> Assertions.assertEquals(saved.getAddress(), workerLocation.getAddress())
 		);
 
 	}
