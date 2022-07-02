e1ed61d15888615117ad4b367bdfe7313a6b25ef
SeungYoung Oh <seung-00@naver.com>
2022-07-02 17:46:18 +0900

    feat: oauthId 유니크 인덱스 추가

--- src/main/java/com/workat/domain/user/User.java
+++ src/main/java/com/workat/domain/user/User.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
 
 import com.workat.domain.auth.OauthType;
 
@@ -16,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(indexes = @Index(columnList = "oauthId", unique = true))
 public class User {
 
 	@Id
