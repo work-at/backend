34ea2df3fc80db30914f43298dfd892c7a8fee45
Soyeon Park <sypark9646@gmail.com>
2022-07-05 15:46:22 +0900

    refactor: request mapping style 통일

--- src/main/java/com/workat/api/map/controller/LocationBatchController.java
+++ src/main/java/com/workat/api/map/controller/LocationBatchController.java
@@ -1,14 +1,11 @@
 package com.workat.api.map.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.workat.api.map.dto.LocationResponse;
 import com.workat.api.map.dto.LocationUpdateRequest;
 import com.workat.api.map.service.LocationService;
-
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
--- src/main/java/com/workat/api/map/controller/WorkChatController.java
+++ src/main/java/com/workat/api/map/controller/WorkChatController.java
@@ -3,7 +3,6 @@ package com.workat.api.map.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.workat.api.map.dto.WorkerDetailResponse;
@@ -11,20 +10,19 @@ import com.workat.api.map.dto.WorkerResponse;
 import com.workat.api.map.service.WorkChatService;
 import lombok.RequiredArgsConstructor;
 
-@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @RestController
 public class WorkChatController {
 
 	private final WorkChatService workChatService;
 
-	@GetMapping("/map/workers/{userId}/detail")
+	@GetMapping("/api/v1/map/workers/{userId}/detail")
 	public ResponseEntity<WorkerDetailResponse> findWorkerDetailById(@PathVariable("userId") Long userId) {
 		WorkerDetailResponse response = workChatService.findWorkerDetailById(userId);
 		return ResponseEntity.ok(response);
 	}
 
-	@GetMapping("/map/workers/{userId}")
+	@GetMapping("/api/v1/map/workers/{userId}")
 	public ResponseEntity<WorkerResponse> findWorkerById(@PathVariable("userId") Long userId) {
 		WorkerResponse response = workChatService.findWorkerById(userId);
 		return ResponseEntity.ok(response);
