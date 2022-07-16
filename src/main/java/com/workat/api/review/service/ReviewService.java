588d8531918c0f4fac630f44928a7fe65415cdbb
SeungYoung Oh <seung-00@naver.com>
2022-07-16 16:44:34 +0900

    feat: ReviewService 수정
    * user 가 리뷰 남겼는지 체크
    * 내부 로직 수정

--- src/main/java/com/workat/api/review/service/ReviewService.java
+++ src/main/java/com/workat/api/review/service/ReviewService.java
@@ -3,6 +3,7 @@ package com.workat.api.review.service;
 import static java.util.stream.Collectors.*;
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -11,8 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.workat.api.review.dto.CafeReviewDto;
+import com.workat.api.review.dto.CafeReviewsDto;
 import com.workat.api.review.dto.request.CafeReviewRequest;
-import com.workat.api.review.dto.response.CafeReviewsResponse;
 import com.workat.common.exception.BadRequestException;
 import com.workat.common.exception.NotFoundException;
 import com.workat.domain.map.entity.Location;
@@ -35,37 +36,54 @@ public class ReviewService {
 	private final LocationRepository locationRepository;
 
 	@Transactional(readOnly = true)
-	public CafeReviewsResponse getCafeReviews(long locationId) {
-
+	public CafeReviewsDto getCafeReviews(long locationId, Users user) {
 		final List<CafeReview> cafeReviews = cafeReviewRepository.findAllByLocation_Id(locationId);
 
-		final Map<CafeReviewType, Long> reviewCountMap = cafeReviews.stream()
-																	.collect(
-																		groupingBy(CafeReview::getReviewType,
-																			counting()));
+		final HashMap<CafeReviewType, Long> cafeReviewCountMap = convertReviewCountMap(cafeReviews);
+		final List<CafeReviewDto> sortedReviewDtos = getSortedReviewDtos(cafeReviewCountMap);
 
-		final List<CafeReviewDto> cafeReviewDtos = reviewCountMap.entrySet()
-																 .stream()
-																 .map(entry ->
-																	 CafeReviewDto.of(
-																		 entry.getKey(),
-																		 entry.getValue()))
-																 .sorted(
-																	 // count 역순으로 정렬
-																	 Comparator.comparingLong(CafeReviewDto::getCount)
-																			   .reversed())
-																 .collect(toList());
+		final boolean userReviewed = checkUserReviewed(cafeReviews, user);
 
-		return CafeReviewsResponse.of(cafeReviewDtos);
+		return CafeReviewsDto.of(
+			sortedReviewDtos,
+			userReviewed
+		);
+	}
+
+	// TODO: 추후 Review 상속 관계 고려해보기
+	private HashMap<CafeReviewType, Long> convertReviewCountMap(List<CafeReview> reviews) {
+
+		final HashMap<CafeReviewType, Long> reviewCountMap = reviews.stream()
+			.collect(groupingBy(CafeReview::getReviewType, HashMap::new, counting()));
+
+		return reviewCountMap;
+	}
+
+	private List<CafeReviewDto> getSortedReviewDtos(Map<CafeReviewType, Long> map) {
+
+		return map.entrySet()
+			.stream()
+			.map(entry -> CafeReviewDto.of(
+				entry.getKey(),
+				entry.getValue()))
+			.sorted(Comparator.comparingLong(CafeReviewDto::getCount) // count 역순으로 정렬
+				.reversed())
+			.collect(toList());
+	}
+
+	private boolean checkUserReviewed(List<CafeReview> cafeReviews, Users user) {
+		final Optional<CafeReview> reviewMatchedUser = cafeReviews.stream()
+			.filter(review -> review.getUser() == user)
+			.findFirst();
+
+		return reviewMatchedUser.isPresent();
 	}
 
 	@Transactional
 	public void addCafeReview(long locationId, CafeReviewRequest cafeReviewRequest, Users user) {
 
 		final Location location = locationRepository.findById(locationId)
-													.orElseThrow(
-														() -> new NotFoundException(
-															"No location found for the id"));
+			.orElseThrow(() -> new NotFoundException("No location found for the id"));
 
 		final Optional<CafeReview> optionalCafeReview = cafeReviewRepository.findByLocation_IdAndUser_Id(
 			locationId,
@@ -78,12 +96,10 @@ public class ReviewService {
 		final List<String> reviewTypeNames = cafeReviewRequest.getReviewTypeNames();
 
 		final List<CafeReview> cafeReviews = reviewTypeNames.stream()
-															.map(name -> CafeReviewType.of(name))
-															.distinct()
-															.map(
-																reviewType -> CafeReview.of(reviewType, location,
-																	user))
-															.collect(toList());
+			.map(name -> CafeReviewType.of(name))
+			.distinct()
+			.map(reviewType -> CafeReview.of(reviewType, location, user))
+			.collect(toList());
 
 		cafeReviewRepository.saveAll(cafeReviews);
 	}
