a229544a4b7d950480023d3abf0532ffefeea396
SeungYoung Oh <seung-00@naver.com>
2022-07-17 15:51:58 +0900

    feat: ReviewServiceTest 수정
    * Review 변경 사항 ReviewServiceTest 에 반영
    * addRestaurantReview 테스트 추가

--- src/test/java/com/workat/api/review/service/ReviewServiceTest.java
+++ src/test/java/com/workat/api/review/service/ReviewServiceTest.java
@@ -18,9 +18,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import com.workat.api.review.dto.CafeReviewDto;
-import com.workat.api.review.dto.CafeReviewsDto;
-import com.workat.api.review.dto.request.CafeReviewRequest;
+import com.workat.api.review.dto.ReviewDto;
+import com.workat.api.review.dto.ReviewsDto;
+import com.workat.api.review.dto.request.ReviewRequest;
 import com.workat.common.exception.BadRequestException;
 import com.workat.domain.auth.OauthType;
 import com.workat.domain.config.MysqlContainerBaseTest;
@@ -30,7 +30,9 @@ import com.workat.domain.map.http.dto.KakaoLocalDataDto;
 import com.workat.domain.map.repository.location.LocationRepository;
 import com.workat.domain.review.CafeReviewType;
 import com.workat.domain.review.entity.CafeReview;
+import com.workat.domain.review.entity.RestaurantReview;
 import com.workat.domain.review.repository.CafeReviewRepository;
+import com.workat.domain.review.repository.RestaurantReviewRepository;
 import com.workat.domain.user.entity.Users;
 import com.workat.domain.user.job.DepartmentType;
 import com.workat.domain.user.job.DurationType;
@@ -47,6 +49,9 @@ public class ReviewServiceTest extends MysqlContainerBaseTest {
 	private CafeReviewRepository cafeReviewRepository;
 
 	@Autowired
+	private RestaurantReviewRepository restaurantReviewRepository;
+
+	@Autowired
 	private LocationRepository locationRepository;
 
 	@Autowired
@@ -54,10 +59,10 @@ public class ReviewServiceTest extends MysqlContainerBaseTest {
 
 	@BeforeEach
 	void setUp() {
-		this.reviewService = new ReviewService(cafeReviewRepository, locationRepository);
+		this.reviewService = new ReviewService(cafeReviewRepository, restaurantReviewRepository, locationRepository);
 	}
 
-	private List<Location> saveLocations(int size) {
+	private List<Location> saveLocations(int size, LocationCategory category) {
 		return IntStream.range(0, size)
 			.mapToObj(idx -> {
 					String value = String.valueOf(idx);
@@ -74,7 +79,7 @@ public class ReviewServiceTest extends MysqlContainerBaseTest {
 						.build();
 
 					Location location = Location.builder()
-						.category(LocationCategory.CAFE)
+						.category(category)
 						.dto(dto)
 						.build();
 
@@ -103,11 +108,11 @@ public class ReviewServiceTest extends MysqlContainerBaseTest {
 			).collect(Collectors.toList());
 	}
 
-	@DisplayName("getCafeReviews 메소드는 리뷰 개수, 타입이 저장한 것과 일치해야 한다")
+	@DisplayName("getLocationReviews() 메소드는 리뷰 개수, 타입이 저장한 것과 일치해야 한다")
 	@Test
 	void reviewTypeSize() {
 		// given
-		final Location location = saveLocations(1).get(0);
+		final Location location = saveLocations(1, LocationCategory.CAFE).get(0);
 		final List<Users> users = saveUsers(3);
 
 		final Users user1 = users.get(0);
@@ -123,14 +128,13 @@ public class ReviewServiceTest extends MysqlContainerBaseTest {
 
 		// when
 		final long locationId = location.getId();
-		final List<CafeReviewDto> cafeReviewDtos = reviewService.getCafeReviews(locationId, user1)
-			.getCafeReviews();
+		final List<ReviewDto> reviewDtos = reviewService.getLocationReviews(locationId, user1.getId()).getReviewDtos();
 
-		final Map<String, Long> reviewCountMap = cafeReviewDtos.stream()
+		final Map<String, Long> reviewCountMap = reviewDtos.stream()
 			.collect(
 				Collectors.toMap(
 					dto -> dto.getReviewTypeDto().getName(),
-					CafeReviewDto::getCount));
+					ReviewDto::getCount));
 		// then
 		assertAll(
 			// PARKING 2, WIFI 1 저장 됐는지 확인
@@ -139,11 +143,11 @@ public class ReviewServiceTest extends MysqlContainerBaseTest {
 		);
 	}
 
-	@DisplayName("getCafeReviews 메소드는 cafe 리뷰들을 count 역순으로 정렬해야 한다")
+	@DisplayName("getLocationReviews() 메소드는 location 리뷰들을 count 역순으로 정렬해야 한다")
 	@Test
-	void getCafeReviews_sorted() {
+	void getLocationReviews_sorted() {
 		// given
-		final Location location = saveLocations(1).get(0);
+		final Location location = saveLocations(1, LocationCategory.CAFE).get(0);
 		final List<Users> users = saveUsers(20);
 
 		List<CafeReview> cafeReviews1 = users.subList(0, 10) // 1st 큼
@@ -169,24 +173,24 @@ public class ReviewServiceTest extends MysqlContainerBaseTest {
 		// when
 		final long locationId = location.getId();
 		final Users user = users.get(0);
-		final List<CafeReviewDto> cafeReviewDtos = reviewService.getCafeReviews(locationId, user).getCafeReviews();
+		final List<ReviewDto> reviewDtos = reviewService.getLocationReviews(locationId, user.getId()).getReviewDtos();
 
 		// then
-		assertEquals(cafeReviewDtos.size(), 3); // PARKING, WIFI, NIGHT_VIEW
+		assertEquals(reviewDtos.size(), 3); // PARKING, WIFI, NIGHT_VIEW
 
-		for (int i = 0; i < cafeReviewDtos.size() - 1; i++) {
-			CafeReviewDto curDto = cafeReviewDtos.get(i);
-			CafeReviewDto nextDto = cafeReviewDtos.get(i + 1);
+		for (int i = 0; i < reviewDtos.size() - 1; i++) {
+			ReviewDto curDto = reviewDtos.get(i);
+			ReviewDto nextDto = reviewDtos.get(i + 1);
 
 			assertTrue(curDto.getCount() >= nextDto.getCount()); // 역순 정렬
 		}
 	}
 
-	@DisplayName("getCafeReviews 메소드는 user 가 해당 cafe 에 리뷰를 남겼는지 여부를 알려줘야 한다")
+	@DisplayName("getLocationReviews() 메소드는 user 가 해당 location 에 리뷰를 남겼는지 여부를 알려줘야 한다")
 	@Test
-	void getCafeReviews_userReviewed() {
+	void getLocationReviews_userReviewed() {
 		// given
-		final Location location = saveLocations(1).get(0);
+		final Location location = saveLocations(1, LocationCategory.CAFE).get(0);
 		final List<Users> users = saveUsers(3);
 
 		final Users user1 = users.get(0);
@@ -196,21 +200,21 @@ public class ReviewServiceTest extends MysqlContainerBaseTest {
 		final CafeReview cafeReview1 = CafeReview.of(CafeReviewType.PARKING, location, user1);
 		final CafeReview cafeReview2 = CafeReview.of(CafeReviewType.WIFI, location, user2);
 
-		// user1, user2 만 카페 리뷰 저장
+		// user1, user2 만 리뷰 저장
 		cafeReviewRepository.saveAll(Arrays.asList(cafeReview1, cafeReview2));
 
 		// when
 		final long locationId = location.getId();
 
-		final CafeReviewsDto cafeReviews1 = reviewService.getCafeReviews(locationId, user1);
-		final CafeReviewsDto cafeReviews2 = reviewService.getCafeReviews(locationId, user2);
-		final CafeReviewsDto cafeReviews3 = reviewService.getCafeReviews(locationId, user3);
+		final ReviewsDto locationReviews1 = reviewService.getLocationReviews(locationId, user1.getId());
+		final ReviewsDto locationReviews2 = reviewService.getLocationReviews(locationId, user2.getId());
+		final ReviewsDto locationReviews3 = reviewService.getLocationReviews(locationId, user3.getId());
 
 		// then
 		assertAll(
-			() -> assertTrue(cafeReviews1.isUserReviewed()),
-			() -> assertTrue(cafeReviews2.isUserReviewed()),
-			() -> assertFalse(cafeReviews3.isUserReviewed()) // user3 은 리뷰 저장 안 됨
+			() -> assertTrue(locationReviews1.isUserReviewed()),
+			() -> assertTrue(locationReviews2.isUserReviewed()),
+			() -> assertFalse(locationReviews3.isUserReviewed()) // user3 은 리뷰 저장 안 됨
 		);
 	}
 
@@ -218,10 +222,10 @@ public class ReviewServiceTest extends MysqlContainerBaseTest {
 	@Test
 	void addCafeReview_size() {
 		// given
-		final Long locationId = saveLocations(1).get(0).getId();
+		final Long locationId = saveLocations(1, LocationCategory.CAFE).get(0).getId();
 		final Users user = saveUsers(1).get(0);
 
-		CafeReviewRequest reviewRequest = CafeReviewRequest.of(
+		ReviewRequest reviewRequest = ReviewRequest.of(
 			new HashSet<>(Arrays.asList("PARKING", "WIFI", "NIGHT_VIEW")));
 
 		// when
@@ -229,21 +233,21 @@ public class ReviewServiceTest extends MysqlContainerBaseTest {
 
 		// then
 		final Long userId = user.getId();
-		List<CafeReview> allByLocation_idAndUser_id = cafeReviewRepository.findAllByLocation_IdAndUser_Id(locationId,
+		List<CafeReview> cafeReviews = cafeReviewRepository.findAllByLocation_IdAndUser_Id(locationId,
 			userId);
 
-		assertTrue(allByLocation_idAndUser_id.size() == 3);
+		assertEquals(cafeReviews.size(), 3);
 	}
 
 	@DisplayName("addCafeReview 메소드는 중복된 유저 id 로 추가하는 경우 실패해야 한다")
 	@Test
 	void addCafeReview_fail_duplicate_user() {
 		// given
-		final Location location = saveLocations(1).get(0);
+		final Location location = saveLocations(1, LocationCategory.CAFE).get(0);
 		final Users user = saveUsers(1).get(0);
 
-		CafeReviewRequest reviewRequest1 = CafeReviewRequest.of(new HashSet<>(Arrays.asList("PARKING")));
-		CafeReviewRequest reviewRequest2 = CafeReviewRequest.of(new HashSet<>(Arrays.asList("WIFI")));
+		ReviewRequest reviewRequest1 = ReviewRequest.of(new HashSet<>(Arrays.asList("NOT_CROWDED")));
+		ReviewRequest reviewRequest2 = ReviewRequest.of(new HashSet<>(Arrays.asList("EAT_ALONE")));
 
 		// when
 
@@ -252,6 +256,47 @@ public class ReviewServiceTest extends MysqlContainerBaseTest {
 		assertThrows(BadRequestException.class, () -> {
 			reviewService.addCafeReview(locationId, reviewRequest1, user);
 			reviewService.addCafeReview(locationId, reviewRequest2, user);
+		});
+	}
+
+	@DisplayName("addRestaurantReview 메소드는 인자로 주어진 리뷰 타입 개수만큼 리뷰를 저장한다")
+	@Test
+	void addRestaurantReview_size() {
+		// given
+		final Long locationId = saveLocations(1, LocationCategory.RESTAURANT).get(0).getId();
+		final Users user = saveUsers(1).get(0);
+
+		ReviewRequest reviewRequest = ReviewRequest.of(
+			new HashSet<>(Arrays.asList("QUICK_FOOD", "NOT_CROWDED", "SNACK")));
+
+		// when
+		reviewService.addRestaurantReview(locationId, reviewRequest, user);
+
+		// then
+		final Long userId = user.getId();
+		List<RestaurantReview> restaurantReviews = restaurantReviewRepository.findAllByLocation_IdAndUser_Id(locationId,
+			userId);
+
+		assertEquals(restaurantReviews.size(), 3);
+	}
+
+	@DisplayName("addRestaurantReview 메소드는 중복된 유저 id 로 추가하는 경우 실패해야 한다")
+	@Test
+	void addRestaurantReview_fail_duplicate_user() {
+		// given
+		final Location location = saveLocations(1, LocationCategory.RESTAURANT).get(0);
+		final Users user = saveUsers(1).get(0);
+
+		ReviewRequest reviewRequest1 = ReviewRequest.of(new HashSet<>(Arrays.asList("QUICK_FOOD")));
+		ReviewRequest reviewRequest2 = ReviewRequest.of(new HashSet<>(Arrays.asList("EAT_ALONE")));
+
+		// when
+
+		// then
+		final long locationId = location.getId();
+		assertThrows(BadRequestException.class, () -> {
+			reviewService.addRestaurantReview(locationId, reviewRequest1, user);
+			reviewService.addRestaurantReview(locationId, reviewRequest2, user);
 		});
 	}
 }
