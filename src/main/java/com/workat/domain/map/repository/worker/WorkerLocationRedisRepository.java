667fdc812e9d116779efa2b4985289914dd6d850
Soyeon Park <sypark9646@gmail.com>
2022-07-17 23:43:50 +0900

    feat: interface projection

--- src/main/java/com/workat/domain/map/repository/worker/WorkerLocationRedisRepository.java
+++ src/main/java/com/workat/domain/map/repository/worker/WorkerLocationRedisRepository.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Point;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.workat.api.map.dto.WorkerPinDto;
 import com.workat.domain.map.entity.WorkerLocation;
 
 public interface WorkerLocationRedisRepository extends CrudRepository<WorkerLocation, Long> {
+
+	@Query(
+		value = "GEORADIUS worker:location :point :kilometer km WITHDIST",
+		nativeQuery = true)
+	List<WorkerPinDto> findWorkerPinsByLocationNear(Point point, double kilometer);
 
 	List<WorkerLocation> findAllByLocationNear(Point point, Distance distance);
 }
