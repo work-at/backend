19c13c078631f0e0313874b2dbdc5d50e05894e6
Soyeon Park <sypark9646@gmail.com>
2022-07-22 19:38:11 +0900

    refactor: 로깅 삭제

--- src/main/java/com/workat/common/util/FileUploadUtils.java
+++ src/main/java/com/workat/common/util/FileUploadUtils.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 
 import org.springframework.util.FileCopyUtils;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class FileUploadUtils {
 
 	public static String fileUpload(String uploadPath, String fileName, byte[] fileData) throws IOException {
