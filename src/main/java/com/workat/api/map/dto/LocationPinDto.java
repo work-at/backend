293c83c250365450a6be3f467c977c5af475dfa0
dev-donghwan <ycdoit@naver.com>
2022-08-04 23:35:48 +0900

    fix: 메소드 필드 수정

--- src/main/java/com/workat/api/map/dto/LocationPinDto.java
+++ src/main/java/com/workat/api/map/dto/LocationPinDto.java
@@ -8,11 +8,11 @@ import lombok.NoArgsConstructor;
 @Getter
 public class LocationPinDto extends LocationDto {
 
-	public LocationPinDto(long id, String placeId, double latitude, double longitude) {
-		super(id, placeId, latitude, longitude);
+	public LocationPinDto(long id, String placeId, double longitude, double latitude) {
+		super(id, placeId, longitude, latitude);
 	}
 
-	public static LocationPinDto of(long id, String placeId, double latitude, double longitude) {
-		return new LocationPinDto(id, placeId, latitude, longitude);
+	public static LocationPinDto of(long id, String placeId, double longitude, double latitude) {
+		return new LocationPinDto(id, placeId, longitude, latitude);
 	}
 }
