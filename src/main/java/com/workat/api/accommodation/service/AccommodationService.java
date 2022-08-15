ddb911e5888efb02041dc20c3fd4a62116ef8978
SeungYoung Oh <seung-00@naver.com>
2022-08-16 02:54:49 +0900

    feat: AccommodationService 추가

---
+++ src/main/java/com/workat/api/accommodation/service/AccommodationService.java
@@ -0,0 +1,59 @@
+package com.workat.api.accommodation.service;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Service;
+
+import com.workat.api.accommodation.dto.AccommodationDto;
+import com.workat.api.accommodation.dto.response.AccommodationsResponse;
+import com.workat.domain.accommodation.RegionType;
+import com.workat.domain.tag.AccommodationInfoTag;
+import com.workat.domain.tag.AccommodationReviewTag;
+import com.workat.domain.tag.dto.TagDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class AccommodationService {
+
+	public AccommodationsResponse getAccommodations(
+		RegionType region,
+		AccommodationInfoTag infoTagName,
+		AccommodationReviewTag accommodationReviewTag,
+		int pageNumber,
+		int pageSize
+	) {
+
+		return AccommodationsResponse.of(
+			Arrays.asList(AccommodationDto.of(1L, "그랜드워커힐서울", 235000L,
+					"https://lh5.googleusercontent.com/p/AF1QipOKO_7oTuHLK31fOjhqp13KompnHRxgi_2_oOVT=w253-h168-k-no",
+					Arrays.asList(
+						TagDto.of(AccommodationInfoTag.NEAR_CITY),
+						TagDto.of(AccommodationInfoTag.WORKSPACE),
+						TagDto.of(AccommodationInfoTag.SHARED_WORKSPACE)
+					),
+					Arrays.asList(
+						TagDto.of(AccommodationReviewTag.FOCUS),
+						TagDto.of(AccommodationReviewTag.SERVE_MEAL),
+						TagDto.of(AccommodationReviewTag.WIFI)
+					)
+				),
+				AccommodationDto.of(2L, "씨마크호텔", 135000L,
+					"https://lh5.googleusercontent.com/proxy/pxIAn34FA3bpLmWfDBKZe6uTiFdb7JrocuP7tzcLTTWcINIqCKLsuADqZW65VteN0bZ28rWStDjNwGjBNhr4_V8KHjBW7aWhNkORBP3Jw9UFmeqive-omWDVIUh5HwVj29V9wi_7iOoUKcvCG6XduQ6Bl2MYyQ=w253-h184-k-no",
+					Arrays.asList(
+						TagDto.of(AccommodationInfoTag.NEAR_SEA),
+						TagDto.of(AccommodationInfoTag.FREE_PARKING),
+						TagDto.of(AccommodationInfoTag.WORKSPACE)
+					),
+					Arrays.asList(
+						TagDto.of(AccommodationReviewTag.WIFI),
+						TagDto.of(AccommodationReviewTag.POWER),
+						TagDto.of(AccommodationReviewTag.SERVE_MEAL)
+					)
+				)
+			), pageNumber, pageSize, 150
+		);
+	}
+
+}
