28b005482251f4dd510ebfdcba7045855dac0fbe
SeungYoung Oh <seung-00@naver.com>
2022-08-16 02:45:30 +0900

    feat: AccommodationReviewDto 추가

---
+++ src/main/java/com/workat/api/accommodation/dto/AccommodationReviewDto.java
@@ -0,0 +1,34 @@
+package com.workat.api.accommodation.dto;
+
+import java.util.List;
+
+import com.workat.domain.tag.dto.TagWithCountDto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class AccommodationReviewDto {
+
+	@ApiModelProperty(name = "reviews")
+	private List<TagWithCountDto> reviews;
+
+	@ApiModelProperty(name = "userReviewed", notes = "유저가 리뷰를 남겼는지 여부")
+	private boolean userReviewed;
+
+	@ApiModelProperty(name = "userCount", notes = "리뷰 남긴 유저 수")
+	private int userCount;
+
+	private AccommodationReviewDto(List<TagWithCountDto> reviews, boolean userReviewed, int userCount) {
+		this.reviews = reviews;
+		this.userReviewed = userReviewed;
+		this.userCount = userCount;
+	}
+
+	public static AccommodationReviewDto of(List<TagWithCountDto> reviews, boolean userReviewed, int userCount) {
+		return new AccommodationReviewDto(reviews, userReviewed, userCount);
+	}
+}
