fa36e8c6a51b07eafcce961018ccca635e794f83
Soyeon Park <sypark9646@gmail.com>
2022-08-22 18:23:08 +0900

    remove: delete unused field

--- src/main/java/com/workat/domain/tour/RegionCode.java
+++ src/main/java/com/workat/domain/tour/RegionCode.java
@@ -5,19 +5,17 @@ import java.util.Set;
 
 public enum RegionCode {
 
-	SEOUL("11", "/metcoRegnVisitrDDList"),
-	JEJU("50", "/metcoRegnVisitrDDList"),
-	GANGNEUNG("42150", "/locgoRegnVisitrDDList"),
-	SOKCHO("42210", "/locgoRegnVisitrDDList");
+	SEOUL("11"),
+	JEJU("50"),
+	GANGNEUNG("42150"),
+	SOKCHO("42210");
 
 	private String code;
-	private String endpoint;
 
 	public static final Set<RegionCode> ALL = EnumSet.allOf(RegionCode.class);
 
-	RegionCode(String code, String endpoint) {
+	RegionCode(String code) {
 		this.code = code;
-		this.endpoint = endpoint;
 	}
 
 	public String getCode() {
