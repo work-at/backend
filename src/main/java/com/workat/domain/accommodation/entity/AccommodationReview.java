f45f48a40bb6a11cfca4f48635c5b213068c1ed0
SeungYoung Oh <seung-00@naver.com>
2022-08-26 23:02:00 +0900

    feat: AccommodationReview entity 추가

---
+++ src/main/java/com/workat/domain/accommodation/entity/AccommodationReview.java
@@ -0,0 +1,53 @@
+package com.workat.domain.accommodation.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.workat.domain.tag.AccommodationReviewTag;
+import com.workat.domain.user.entity.Users;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class AccommodationReview {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@JoinColumn(name = "accommodation_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Accommodation accommodation;
+
+	@JoinColumn(name = "user_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Users user;
+
+	@Enumerated(EnumType.STRING)
+	private AccommodationReviewTag tag;
+
+	private AccommodationReview(Accommodation accommodation,
+		Users user,
+		AccommodationReviewTag tag) {
+		this.accommodation = accommodation;
+		this.user = user;
+		this.tag = tag;
+	}
+
+	public static AccommodationReview of(Accommodation accommodation,
+		Users user,
+		AccommodationReviewTag tag) {
+		return new AccommodationReview(accommodation, user, tag);
+	}
+}
