c2108adac2043dd85f70bf0727c71e451a4083b0
SeungYoung Oh <seung-00@naver.com>
2022-08-30 18:12:11 +0900

    feat: AccommodationReview repository 추가

---
+++ src/main/java/com/workat/domain/accommodation/repository/AccommodationReviewRepository.java
@@ -0,0 +1,13 @@
+package com.workat.domain.accommodation.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.workat.domain.accommodation.entity.AccommodationReview;
+
+public interface AccommodationReviewRepository extends JpaRepository<AccommodationReview, Long> {
+
+	List<AccommodationReview> findAllByAccommodation_Id(Long accommodationId);
+
+}
