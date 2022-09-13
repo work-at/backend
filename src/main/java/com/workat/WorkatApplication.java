dab55925ea3de83711546477e0c0bdb40deea3eb
dev-donghwan <ycdoit@naver.com>
2022-09-13 16:48:00 +0900

    feat: timezone 변경

--- src/main/java/com/workat/WorkatApplication.java
+++ src/main/java/com/workat/WorkatApplication.java
@@ -1,5 +1,7 @@
 package com.workat;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkatApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(WorkatApplication.class, args);
 	}
 
