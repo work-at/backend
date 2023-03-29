c69ee7fd17d847350d7e88d6b6d0c25efb3655f9
SeungYoung Oh <seung-00@naver.com>
2023-03-29 19:44:33 +0900

    refactor: AddressService 의 메서드 리팩터링
    * getAddressAndSave 메서드를 분리했습니다.
    * KakaoAddressResponse 에 String address 로 변환하는 로직을 위임했습니다.

--- src/main/java/com/workat/domain/map/http/dto/KakaoAddressResponse.java
+++ src/main/java/com/workat/domain/map/http/dto/KakaoAddressResponse.java
@@ -1,6 +1,11 @@
 package com.workat.domain.map.http.dto;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+
+import com.workat.common.exception.base.BusinessException;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,4 +19,12 @@ public class KakaoAddressResponse {
 
 	private List<KakaoAddressDocumentDto> documents;
 
+	public String convertToAddress() {
+		return Optional.of(this)
+			.map(KakaoAddressResponse::getDocuments)
+			.map(d -> d.get(0))
+			.map(KakaoAddressDocumentDto::getAddress)
+			.map(KakaoAddressDto::getEntireAddress)
+			.orElseThrow(() -> new BusinessException(HttpStatus.NO_CONTENT, "위도 경도를 주소로 바꿀 수 없습니다."));
+	}
 }
